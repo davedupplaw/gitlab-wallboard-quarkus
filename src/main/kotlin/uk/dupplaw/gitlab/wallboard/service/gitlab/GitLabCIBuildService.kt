e286c4e2af2df0aa114e5e055be325dc7ffda9e0
Dave Dupplaw <david.dupplaw@siemens.com>
2023-09-28 14:29:21 +0100

    feat: DD: Include bridges in reason job list

--- src/main/kotlin/uk/dupplaw/gitlab/wallboard/service/gitlab/GitLabCIBuildService.kt
+++ src/main/kotlin/uk/dupplaw/gitlab/wallboard/service/gitlab/GitLabCIBuildService.kt
@@ -25,6 +25,7 @@ class GitLabCIBuildService(
         "$baseUrl/projects/$PROJECT_ID_TAG/pipelines?simple=true&per_page=1&order_by=id&sort=desc&ref=:ref"
     private val pipelineUrl = "$baseUrl/projects/$PROJECT_ID_TAG/pipelines/:pipelineId?simple=true"
     private val jobUrl = "$baseUrl/projects/$PROJECT_ID_TAG/pipelines/:pipelineId/jobs?per_page=3&scope=:scope"
+    private val bridgesUrl = "$baseUrl/projects/$PROJECT_ID_TAG/pipelines/:pipelineId/bridges?per_page=3&scope=:scope"
 
     override fun retrieveBuildInformation(project: Project) =
         getLatestBuild(project.id)?.let {
@@ -87,7 +88,9 @@ class GitLabCIBuildService(
                     lastBuildTimestamp = lastBuild,
                     user = user,
                     textStatus = actualStatus,
-                    currentStatusReasons = scope?.let { getJobInfo(projectId, id, it) } ?: listOf()
+                    currentStatusReasons = (scope?.let {
+                        getBridgeInfo(projectId, id, it) + getJobInfo(projectId, id, it)
+                    } ?: listOf()).take(3)
                 )
             }
         }
@@ -103,6 +106,29 @@ class GitLabCIBuildService(
             logger.info { "Getting job info: $jobUrl" }
 
             return URL(jobUrl).openConnection().apply {
+                readTimeout = 800
+                connectTimeout = 200
+                setRequestProperty(PRIVATE_TOKEN_HEADER, gitLabCIBuildServiceConfiguration.token)
+            }.getInputStream().use { ins ->
+                ObjectMapper().readTree(ins).map { it["name"].asText() }
+            }
+        } catch (e: Exception) {
+            logger.warn { "Error getting job information" }
+            logger.warn { e }
+            return listOf()
+        }
+    }
+
+    fun getBridgeInfo(projectId: Long, pipelineId: Long, scope: String): List<String> {
+        try {
+            val bridgesUrl = "https://${gitLabCIBuildServiceConfiguration.host}$bridgesUrl"
+                .replace(PROJECT_ID_TAG, projectId.toString())
+                .replace(":pipelineId", pipelineId.toString())
+                .replace(":scope", scope)
+
+            logger.info { "Getting job info: $bridgesUrl" }
+
+            return URL(bridgesUrl).openConnection().apply {
                 readTimeout = 800
                 connectTimeout = 200
                 setRequestProperty(PRIVATE_TOKEN_HEADER, gitLabCIBuildServiceConfiguration.token)
